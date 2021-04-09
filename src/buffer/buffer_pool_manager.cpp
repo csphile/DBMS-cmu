193bb988a59a4790f8e516c9dd609e5201e84cba
csphile <wenjun.liu@polytechnique.edu>
2021-04-09 20:09:58 +0200

    Restructure

--- src/buffer/buffer_pool_manager.cpp
+++ src/buffer/buffer_pool_manager.cpp
@@ -42,14 +42,72 @@ Page *BufferPoolManager::FetchPageImpl(page_id_t page_id) {
   // 2.     If R is dirty, write it back to the disk.
   // 3.     Delete R from the page table and insert P.
   // 4.     Update P's metadata, read in the page content from disk, and then return a pointer to P.
-  return nullptr;
+  std::lock_guard<std::mutex> guard(latch_);
+  // Step 1
+  frame_id_t frame_id;
+  auto it = page_table_.find(page_id);
+  if (it != page_table_.end()) {
+    pages_[it->second].pin_count_++;
+    replacer_->Pin(it->second);
+    return &pages_[it->second];
+  }
+  // 1.2
+  if (!free_list_.empty()) {
+    frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Victim(&frame_id)) {
+      return nullptr;
+    }
+    // 2.0 write it back to the disk;
+    if (pages_[frame_id].is_dirty_) {
+      disk_manager_ -> WritePage(pages_[frame_id].GetPageId(), pages_[frame_id].GetData());
+    }
+    // delete and insert P
+    page_table_.erase(pages_[frame_id].GetPageId());
+  }
+  // 4 read in the page content
+  pages_[frame_id].ResetMemory();
+  disk_manager_->ReadPage(page_id, pages_[frame_id].GetData());
+
+  pages_[frame_id].page_id_ = page_id;
+  page_table_[page_id] = frame_id;
+  // update P's metadata and read in the page content from disk
+  pages_[frame_id].pin_count_++;
+  replacer_->Pin(frame_id);
+  pages_[frame_id].is_dirty_ = false;
+  // return a pointer
+  return &pages_[frame_id];
 }
 
-bool BufferPoolManager::UnpinPageImpl(page_id_t page_id, bool is_dirty) { return false; }
+bool BufferPoolManager::UnpinPageImpl(page_id_t page_id, bool is_dirty) {
+  std::lock_guard<std::mutex> guard(latch_);
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end() || pages_[it->second].pin_count_ <= 0) {
+    return false;
+  }
+  pages_[it->second].pin_count_--;
+  if (is_dirty) {
+	  pages_[it->second].is_dirty_ = true;
+  }
+  if (pages_[it->second].pin_count_ == 0) {
+    replacer_->Unpin(it->second);
+  }
+  return true;
+}
 
 bool BufferPoolManager::FlushPageImpl(page_id_t page_id) {
   // Make sure you call DiskManager::WritePage!
-  return false;
+  std::lock_guard<std::mutex> guard(latch_);
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    return false;
+  }
+  if (pages_[it -> second].is_dirty_){
+    disk_manager_->WritePage(page_id, pages_[it->second].GetData());
+    // pages_[it->second].is_dirty_ = false;
+  }
+  return true;
 }
 
 Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
@@ -58,7 +116,29 @@ Page *BufferPoolManager::NewPageImpl(page_id_t *page_id) {
   // 2.   Pick a victim page P from either the free list or the replacer. Always pick from the free list first.
   // 3.   Update P's metadata, zero out memory and add P to the page table.
   // 4.   Set the page ID output parameter. Return a pointer to P.
-  return nullptr;
+  std::lock_guard<std::mutex> guard(latch_);
+  frame_id_t frame_id;
+  if (!free_list_.empty()) {
+    frame_id = free_list_.front();
+    free_list_.pop_front();
+  } else {
+    if (!replacer_->Victim(&frame_id)) {
+      return nullptr;
+    }
+  }
+  if (pages_[frame_id].is_dirty_){
+  	disk_manager_ -> WritePage(pages_[frame_id].GetPageId(), pages_[frame_id].GetData());
+  }
+  page_table_.erase(pages_[frame_id].GetPageId());
+  // update P's metadata
+  *page_id = disk_manager_->AllocatePage();
+  page_table_[*page_id] = frame_id;
+  pages_[frame_id].ResetMemory();
+  pages_[frame_id].page_id_ = *page_id;
+  pages_[frame_id].pin_count_++;
+  pages_[frame_id].is_dirty_ = false;
+  replacer_->Pin(frame_id);
+  return &pages_[frame_id];
 }
 
 bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
@@ -67,11 +147,30 @@ bool BufferPoolManager::DeletePageImpl(page_id_t page_id) {
   // 1.   If P does not exist, return true.
   // 2.   If P exists, but has a non-zero pin-count, return false. Someone is using the page.
   // 3.   Otherwise, P can be deleted. Remove P from the page table, reset its metadata and return it to the free list.
-  return false;
+  std::lock_guard<std::mutex> guard(latch_);
+
+  auto it = page_table_.find(page_id);
+  if (it == page_table_.end()) {
+    disk_manager_->DeallocatePage(page_id);
+    return true;
+  }
+  if (pages_[it->second].GetPinCount() > 0) {
+    return false;
+  }
+  Page* to_delete = &pages_[it -> second];
+  free_list_.emplace_back(it->second);
+  page_table_.erase(it);
+  to_delete->ResetMemory();
+  to_delete->page_id_ = INVALID_PAGE_ID;
+  to_delete->is_dirty_=false;
+  to_delete->pin_count_=0;
+  return true;
 }
 
 void BufferPoolManager::FlushAllPagesImpl() {
-  // You can do it!
+  for (auto it : page_table_) {
+    FlushPageImpl(it.first);
+  }
 }
 
 }  // namespace bustub
--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,44 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) : max_size(num_pages) {}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+  const std::lock_guard<std::mutex> lock(_lock_data);
+  // remove the lru element from the replacer
+  if (this->Size() == 0) {
+    return false;
+  }
+  id2data.erase(_data.front());
+  *frame_id = _data.front();
+  _data.pop_front();
+  return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  // if frame_id doesn't exist, do nothing
+  // remove from the replacer
+  const std::lock_guard<std::mutex> lock(_lock_data);
+  auto it = id2data.find(frame_id);
+  if (it != id2data.end()) {
+    _data.erase(it->second);
+    id2data.erase(it);
+  }
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  // if frame_id already exists do nothing
+  // otherwise add to the replacer
+  const std::lock_guard<std::mutex> lock(_lock_data);
+  auto it = id2data.find(frame_id);
+  if (it == id2data.end()) {
+    _data.push_back(frame_id);
+    id2data[frame_id] = prev(_data.end());
+  }
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() { return _data.size(); }
 
 }  // namespace bustub
